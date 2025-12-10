f5d8c7c759f56aa2481b6609ecde06be7e8b6117
JayTongue <jaytongue@gmail.com>
2025-12-10 13:10:01 -0600

    finished 10 for real

--- 2025/10_2.py
+++ 2025/10_2.py
@@ -1,7 +1,5 @@
 import ast
-import numpy as np
-from itertools import product
-from tqdm import tqdm
+from z3 import Int, Optimize, Sum, sat
 
 with open('2025/data/10.txt', 'r') as infile:
     data = [i.strip('\n').split(' ') for i in infile.read().splitlines('\n')]
@@ -10,23 +8,33 @@ with open('2025/data/10.txt', 'r') as infile:
     buttons = [list(map(lambda b: b if isinstance(b, tuple) else (b,), butt)) for butt in buttons]
     joltage = [list(map(int, k[-1][1:-1].split(','))) for k in data]
 data = list(zip(lights, buttons, joltage))
-
 sol = 0
-for lights, buttons, joltage in tqdm(data):
-    prob_min = float('inf')
-    butt_arr = np.zeros((len(buttons), len(lights)), dtype=np.int_)
-    for b_count, b in enumerate(buttons):
-        for elem in b:
-            butt_arr[b_count][elem] = 1
-    butt_arr = butt_arr.T
-    joltage = np.array(joltage, dtype=np.int_)
 
-    for perm in list(product(range(13), repeat=len(buttons))):
-        perm = np.array(perm, dtype=np.int_)
-        result = butt_arr @ perm
+for lights, buttons, joltage in data:
+    n_buttons = len(buttons)
+    n_lights = len(joltage)
 
-        if np.array_equal(result, joltage):
-            prob_min = min(prob_min, sum(perm))
+    butt_arr = [[0] * n_buttons for _ in range(n_lights)]
+    for b_idx, b in enumerate(buttons):
+        for light_idx in b:
+            butt_arr[light_idx][b_idx] = 1
+
+    opt = Optimize()
+    perm = [Int(f"p_{i}") for i in range(n_buttons)]
+
+    for p in perm:
+        opt.add(p >= 0, p <= 1000 )
+
+    for i in range(n_lights):
+        opt.add(Sum(butt_arr[i][j] * perm[j] for j in range(n_buttons)) == joltage[i])
+
+    opt.minimize(Sum(perm))
+
+    if opt.check() == sat:
+        model = opt.model()
+        prob_min = sum(model[p].as_long() for p in perm)
+
     sol += prob_min
+
 print(sol)
-        
+
