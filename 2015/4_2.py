16de94593f3085c23bbc34b11f7a8b8d4b394072
JayTongue <142804110+JayTongue@users.noreply.github.com>
2025-12-12 21:57:37 +0000

    finished day 4

---
+++ 2015/4_2.py
@@ -0,0 +1,12 @@
+from hashlib import md5
+
+with open('2015/data/4.txt', 'r') as infile:
+    key = infile.read()
+
+d = 0
+while True:
+    guess = f'{key}{d}'
+    if str(md5(guess.encode()).hexdigest())[:6] == '000000':
+        break
+    d += 1
+print(d)
