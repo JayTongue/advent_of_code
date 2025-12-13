adf13375633e265075286996686f72fc5f8f3a61
JayTongue <jctung2@illinois.edu>
2025-12-13 07:45:51 -0600

    finished day 6

---
+++ 2015/6_2.py
@@ -0,0 +1,21 @@
+with open('2015/data/6.txt', 'r') as infile:
+    data = infile.read().split('\n')
+    all_moves = []
+    for line in data:
+        line = line.split(' ')
+        if len(line) == 4:
+            line = (line[0], tuple(map(int, line[1].split(','))), tuple(map(int, line[3].split(','))))
+        else:
+            line = (line[1], tuple(map(int, line[2].split(','))), tuple(map(int, line[4].split(','))))
+        all_moves.append(line)
+bool_table = [[0 for _ in range(1000)] for _ in range(1000)]
+for command, corner_1, corner_2 in all_moves:
+    for x in range(min(corner_1[0], corner_2[0]), max(corner_1[0], corner_2[0])+1):
+        for y in range(min(corner_2[1], corner_1[1]), max(corner_2[1], corner_1[1])+1):
+                if command == 'toggle':
+                    bool_table[x][y] = bool_table[x][y]+2
+                elif command == 'on':
+                    bool_table[x][y] = bool_table[x][y]+1
+                else:
+                    bool_table[x][y] = max(bool_table[x][y]-1, 0)
+print(sum([sum(r) for r in bool_table]))
