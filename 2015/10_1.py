6a8afe6efcc4fe15528137520501c9b29a5ed4ac
JayTongue <142804110+JayTongue@users.noreply.github.com>
2025-12-16 22:34:05 +0000

    finished day 10

---
+++ 2015/10_1.py
@@ -0,0 +1,15 @@
+with open('2015/data/10.txt', 'r') as infile:
+    data = list(map(int, list(infile.read())))
+    
+for i in range(40):
+    new_iter = []
+    dig = data[0] ; count = 1
+    for d in data[1:]:
+        if d == dig:
+            count += 1
+        else:
+            new_iter.append(count) ; new_iter.append(dig)
+            dig = d ; count = 1
+    new_iter.append(count) ; new_iter.append(dig)
+    data = new_iter
+print(len(data))
