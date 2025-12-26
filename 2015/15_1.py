8cee36b8c305362ff7914ea8bb8167014c748734
heimerdinger <heimerdinger@heimerdinger.heimerdinger>
2025-12-26 08:38:34 -0600

    finished 15... brute force

--- 2015/15_1.py
+++ 2015/15_1.py
@@ -8,14 +8,17 @@ with open('./2015/data/15.txt', 'r') as infile:
         data[d] = [int(i.split(' ')[1]) for i in data[d]]
         matrix.append(data[d])
     matrix = np.array(matrix).T
+print(matrix)
 
 search_space = 100 ; sol = 0
-for i in tqdm(range(0, search_space)):
-    for j in range(0, search_space-i):
-        for k in range(0, search_space -i-j):
-            for l in range(0, search_space-i-j-k):
-                x_vec = np.array([i, j, l, k]).T
+for i in tqdm(range(0, search_space+1)):
+    for j in range(0, search_space-i+1):
+        for k in range(0, search_space -i-j+1):
+            for l in range(0, search_space-i-j-k+1):
+                x_vec = np.array([i, j, k, l]).T
                 y_vec = np.sum(matrix[:-1] * x_vec, axis=1)
+                y_vec = np.maximum(y_vec, 0)
                 if sum(x_vec) == search_space and np.prod(y_vec) > sol:
-                    sol = np.prod(y_vec)
+                    sol = np.prod(y_vec)          
+
 print(sol)
