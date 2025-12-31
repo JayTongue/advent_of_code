e563f47ffd64ed295b8da5518c940cb1c471d17c
heimerdinger <heimerdinger@heimerdinger.heimerdinger>
2025-12-31 15:46:46 -0600

    finished day 24

---
+++ 2015/24_1.py
@@ -0,0 +1,20 @@
+import itertools
+from math import prod
+
+with open('2015/data/24.txt', 'r') as infile:
+    data = sorted(list(map(int, infile.read().split('\n'))), reverse=True)
+
+bucket_count = 3
+bucket_size = sum(data)//bucket_count
+bucket_combos = itertools.combinations(data, 6)
+    
+fits = set()
+for bucket_combo in bucket_combos:
+    if sum(bucket_combo) == bucket_size:
+        fits.add(frozenset(bucket_combo))
+
+qe = float('inf')
+for f in fits:
+    if len(f) == min(map(len, fits)):
+        qe = min(qe, prod(f))
+print(qe)
