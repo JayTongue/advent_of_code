3d9b6dc55bb042b1210d82662a0944121418a9fd
JayTongue <jctung2@illinois.edu>
2026-02-19 09:48:36 -0600

    finished day 6, easy day 2

---
+++ 2016/6_2.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+with open('2016/data/6.txt', 'r') as infile:
+    data = [list(i) for i in infile.read().split('\n')]
+
+sol = []
+for idx in range(len(data[0])):
+    counter = Counter([j[idx] for j in data])
+    sol.append(min(counter, key=counter.get))
+print(''.join(sol))
