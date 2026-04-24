20d923e19c1cfd7c7b5ada3410ba0b5648a0a4cf
JayTongue <jaytongue@gmail.com>
2026-04-24 16:35:55 -0500

    finished day 11, slow aaaaffffff

---
+++ 2016/11_1.py
@@ -0,0 +1,55 @@
+from collections import deque
+from itertools import combinations, product
+import json
+from copy import deepcopy
+
+with open('2016/data/11.json', 'r') as infile:
+    state_dict = json.load(infile)
+
+hash_order = ['elv', 'com', 'cum', 'rum', 'plm', 'prm', 'cog', 'cug', 'rug', 'plg', 'prg']
+start_hash = tuple((state_dict[d] for d in hash_order))
+end = (4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4)
+seen_states = {start_hash}
+
+def next_states(state_dict, seen_states):
+    e_floor = state_dict['elv']
+    moveable = [k for k, v in state_dict.items() if v == e_floor and k != 'elv']
+    moveable = list(map(list, list(combinations(moveable, 1)) + list(combinations(moveable, 2))))
+    floors = (e_floor + 1, e_floor - 1)
+    moves = list(product(moveable, floors))
+    new_states = []
+    for move in moves:
+        valid, state_hash = check_valid(move, state_dict, seen_states)
+        if valid: 
+            new_states.append(state_hash) 
+    return new_states
+
+def check_valid(move, state_dict, seen_states):
+    if not 1 <= move[1] <= 4:
+        return False, ()
+    test_state = deepcopy(state_dict)
+    test_state = {k: move[1] if k in move[0]+['elv'] else v for k, v in test_state.items()}
+    state_hash = tuple((test_state[d] for d in hash_order))
+    for checker_position in range(1, 6):
+        if state_hash[checker_position] != state_hash[checker_position + 5]:
+            if state_hash[checker_position] in state_hash[-5:]:
+                return False, ()
+    if state_hash in seen_states:
+        return False, ()
+
+    return True, state_hash
+
+queue = deque([(start_hash, 0)])
+seen_states = {start_hash}
+
+while queue:
+    state, steps = queue.popleft()
+    if state == end:
+        print(steps)
+        break
+    else:
+        state_dict = {i:j for i, j in zip(hash_order, state)}
+        for next_state in next_states(state_dict, seen_states):
+            if next_state not in seen_states:
+                seen_states.add(next_state)
+                queue.append((next_state, steps +1))
