063eb1451261b8b4f7c07a5b3f85605c3b4c3d6d
JayTongue <jaytongue@gmail.com>
2026-06-02 14:41:18 -0500

    finished day 22

---
+++ 2017/22_2.py
@@ -0,0 +1,53 @@
+size = 500
+grid = [['.' for _ in range(size)] for _ in range(size)]
+
+with open('2017/data/22.txt', 'r') as outfile:
+    puz = outfile.read().split('\n')
+offset = size//2 - len(puz)//2
+
+for row_idx, row in enumerate(puz):
+    for i in range(len(row)):
+        if row[i] == '#':
+            grid[row_idx + offset][offset + i] = '#' 
+
+def turn(current, direction):
+    orientations = ['^', '>', 'v', '<']
+    if direction == 'left':
+        return orientations[orientations.index(current)-1]
+    else:
+        return orientations[(orientations.index(current)+1)%len(orientations)]
+
+def move(current, direction):
+    if direction == '^':
+        return (current[0]-1, current[1])
+    elif direction == '>':
+        return (current[0], current[1]+1)
+    elif direction == 'v':
+        return (current[0]+1, current[1])
+    elif direction == '<':
+        return (current[0], current[1]-1)
+
+virus_pos = (len(grid)//2, len(grid)//2) ; virus_ori = '^'
+activated = 0
+for _ in range(int(1e7)):
+    if grid[virus_pos[0]][virus_pos[1]] == '.':
+        grid[virus_pos[0]][virus_pos[1]] = 'W'
+        new_direction = turn(virus_ori, 'left')
+        moved = move(virus_pos, new_direction)
+    elif grid[virus_pos[0]][virus_pos[1]] == 'W':
+        grid[virus_pos[0]][virus_pos[1]] = '#'
+        new_direction = virus_ori
+        moved = move(virus_pos, new_direction)
+        activated += 1
+    elif grid[virus_pos[0]][virus_pos[1]] == '#':
+        grid[virus_pos[0]][virus_pos[1]] = 'F'
+        new_direction = turn(virus_ori, 'right')
+        moved = move(virus_pos, new_direction)
+    elif grid[virus_pos[0]][virus_pos[1]] == 'F':
+        grid[virus_pos[0]][virus_pos[1]] = '.'
+        new_direction = turn(turn(virus_ori, 'left'), 'left')
+        moved = move(virus_pos, new_direction)
+    
+    virus_pos = moved ; virus_ori = new_direction
+
+print(activated)
