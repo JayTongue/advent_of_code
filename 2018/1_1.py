f9f2e191797d7d6152e5ff449bf0edabcd65918a
JayTongue <jaytongue@gmail.com>
2026-06-09 15:53:05 -0500

    experiments with cffi

--- 2018/1_1.py
+++ 2018/1_1.py
@@ -1,5 +1,47 @@
+# with open('2018/data/1.txt', 'r') as infile:
+#     changes = infile.read().replace('+', '').split('\n')
+# changes = list(map(int, changes))
+
+# print(sum(changes))
+
+import os
+import sys
+from cffi import FFI
+
+script_dir = os.path.dirname(os.path.abspath(__file__)) #pwd
+cffi_dir = os.path.join(script_dir, "cffi") #points to cffi folder
+
+sys.path.insert(0, cffi_dir) # adds a path, cffi_dir, at index 0 of paths
+
+c_mod = "_1_1"
+ffi = FFI()
+
+ffi.cdef("""
+    int sum_ints(int *values, int length);
+""")
+
+ffi.set_source(
+    c_mod,
+    """
+    int sum_ints(int *values, int length) {
+        int total = 0;
+        for (int i = 0; i < length; i++) {
+            total += values[i];
+        }
+        return total;
+    }
+    """
+)
+
+ffi.compile(tmpdir=cffi_dir, verbose=True)
+
+from _1_1 import lib
+
 with open('2018/data/1.txt', 'r') as infile:
-    changes = infile.read().replace('+', '').split('\n')
+    changes = infile.read().replace("+", "").splitlines()
+
 changes = list(map(int, changes))
 
-print(sum(changes))
+c_array = ffi.new("int[]", changes)
+
+print(lib.sum_ints(c_array, len(changes)))
--- 2018/2_1.py
+++ 2018/2_1.py
@@ -1,13 +1,46 @@
-from collections import Counter
+import os, sys
+from cffi import FFI
+
+pwd = os.path.dirname(os.path.abspath(__file__))
+cffi_dir = os.path.join(pwd, 'cffi')
+sys.path.insert(0, cffi_dir)
+
+c_mod = '_2_1'
+ffi = FFI()
+ffi.cdef('''int twos_and_threes(char **boxes, int box_count);''')
+ffi.set_source(c_mod, '''
+int twos_and_threes(char **boxes, int box_count) {
+    int total_twos = 0;
+    int total_threes = 0;
+
+    for (int i = 0; i < box_count; i++){
+        int counts[26] = {0};
+        for (int j = 0; boxes[i][j] != '\\0'; j++){
+               char c = boxes[i][j];
+
+               if (c >= 'a' && c <= 'z'){counts[c - 'a']++;}
+               }
+               
+               int has_two = 0;
+               int has_three = 0;
+               
+               for (int k = 0; k < 26; k++){
+                    if (counts[k] == 2){has_two = 1;}
+                    if (counts[k] == 3){has_three = 1;}
+               }
+               if (has_two){total_twos++;}
+               if (has_three){total_threes++;}
+               }
+               return total_twos * total_threes;
+               }
+               ''')
+# ffi.compile(tmpdir=cffi_dir, verbose=True)
+
+from _2_1 import lib
 
 with open('2018/data/2.txt', 'r') as infile:
-    boxes = [list(i) for i in infile.read().split('\n')]
+    boxes = infile.read().split('\n')
 
-twos, threes = 0, 0
-for box in boxes:
-    box = Counter(box)
-    if 2 in box.values():
-        twos += 1
-    if 3 in box.values():
-        threes += 1
-print(twos * threes)
+c_boxes = [ffi.new('char[]', box.encode('ascii')) for box in boxes]
+c_box_array = ffi.new('char *[]', c_boxes)
+print(lib.twos_and_threes(c_box_array, len(c_box_array)))
