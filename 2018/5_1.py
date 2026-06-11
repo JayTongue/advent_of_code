ce8f01eb1b8570de956ebfe30e2c06065031e724
JayTongue <jaytongue@gmail.com>
2026-06-11 12:03:24 -0500

    day 4 cffi implementation

--- 2018/5_1.py
+++ 2018/5_1.py
@@ -1,16 +1,66 @@
-with open('2018/data/5.txt', 'r') as infile:
-    poly = list(infile.read())
-case_match = {i: i.upper() for i in list('abcdefghijklmnopqrstuvwxyz')}
+# with open('2018/data/5.txt', 'r') as infile:
+#     poly = list(infile.read())
+# case_match = {i: i.upper() for i in list('abcdefghijklmnopqrstuvwxyz')}
+# case_match |= {v: k for k, v in case_match.items()}
 
-stack = [poly[0]]
-for p in poly[1:]:
-    if not len(stack):
-        stack.append(p)
-    else:
-        if p in case_match.keys() and case_match[p] == stack[-1]:
-            stack.pop()
-        elif stack[-1] in case_match.keys() and case_match[stack[-1]] == p:
-            stack.pop()
-        else:
-            stack.append(p)
-print(len(stack))
+# stack = []
+# for p in poly:
+#     if not len(stack):
+#         stack.append(p)
+#     else:
+#         if case_match[p] == stack[-1]:
+#             stack.pop()
+#         else:
+#             stack.append(p)
+# print(len(stack))
+
+from cffi import FFI
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+cffi_dir = os.path.join(script_dir, 'cffi')
+sys.path.insert(0, cffi_dir)
+
+c_mod = '_5_1'
+ffi = FFI()
+ffi.cdef('''
+         int react(char *poly, int length); 
+         ''')
+ffi.set_source(c_mod, '''
+#include <stdlib.h>
+
+char low_if_high(char c){
+    if (c >= 'A' && c <= 'Z') {return c+32;}
+    return c; 
+}
+
+int reacts(char a, char b) {
+    return a != b && low_if_high(a) == low_if_high(b);               
+}
+
+int react(char *poly, int length) {
+    char *stack = malloc(length);
+    int total_len = 0;
+               
+    for (int i = 0; i < length; i++) {
+        char p = poly[i];
+        if (total_len > 0 && reacts(p, stack[total_len-1])) {total_len--;}
+        else {stack[total_len] = p;
+               total_len++;}
+    }
+    free(stack);
+    return total_len;
+}
+''')
+
+ffi.compile(tmpdir=cffi_dir, verbose=True)
+
+from _5_1 import lib
+
+with open('2018/data/5.txt', 'r') as infile:
+    poly = infile.read().strip()
+
+data = poly.encode('ascii')
+c_poly = ffi.new('char[]', data)
+print(lib.react(c_poly, len(poly)))
--- 2018/5_2.py
+++ 2018/5_2.py
@@ -1,26 +1,56 @@
-import copy
+from cffi import FFI
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+cffi_dir = os.path.join(script_dir, 'cffi')
+sys.path.insert(0, cffi_dir)
+
+c_mod = '_5_1'
+ffi = FFI()
+ffi.cdef('''
+         int react(char *poly, int length); 
+         ''')
+ffi.set_source(c_mod, '''
+#include <stdlib.h>
+
+char low_if_high(char c){
+    if (c >= 'A' && c <= 'Z') {return c+32;}
+    return c; 
+}
+
+int reacts(char a, char b) {
+    return a != b && low_if_high(a) == low_if_high(b);               
+}
+
+int react(char *poly, int length) {
+    char *stack = malloc(length);
+    int total_len = 0;
+               
+    for (int i = 0; i < length; i++) {
+        char p = poly[i];
+        if (total_len > 0 && reacts(p, stack[total_len-1])) {total_len--;}
+        else {stack[total_len] = p;
+               total_len++;}
+    }
+    free(stack);
+    return total_len;
+}
+''')
+
+ffi.compile(tmpdir=cffi_dir, verbose=True)
+
+from _5_1 import lib
 
 with open('2018/data/5.txt', 'r') as infile:
-    perm = list(infile.read())
-case_match = {i: i.upper() for i in list('abcdefghijklmnopqrstuvwxyz')}
+    perm = infile.read().strip()
 
 lowest = float('inf')
-for key in case_match:
-    poly = [i for i in copy.deepcopy(perm) if i not in (key, case_match[key])]
-
-    stack = []
-    for p in poly:
-        if not len(stack):
-            stack.append(p)
-        else:
-            if p in case_match.keys() and case_match[p] == stack[-1]:
-                stack.pop()
-            elif stack[-1] in case_match.keys() and case_match[stack[-1]] == p:
-                stack.pop()
-            else:
-                stack.append(p)
-
-    ans = len(stack)
-    if ans < lowest:
-        lowest = ans
+for low, up in [(i, i.upper()) for i in list('abcdefghijklmnopqrstuvwxyz')]:
+    poly = perm.replace(low, '') ; poly = poly.replace(up, '')
+    data = poly.encode('ascii')
+    c_poly = ffi.new('char[]', data)
+    length = (lib.react(c_poly, len(poly)))
+    if length < lowest:
+        lowest = length
 print(lowest)
