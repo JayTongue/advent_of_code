78bce230febb7b1b1bbf5f83a6833469bb0dff4d
JayTongue <jaytongue@gmail.com>
2026-06-15 14:49:58 -0500

    finished day 7

--- 2018/7_2.py
+++ 2018/7_2.py
@@ -1,14 +1,4 @@
 from collections import defaultdict
-from pprint import pprint
-
-steps = '''Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.
-'''.splitlines()
 
 with open('2018/data/7.txt', 'r') as infile:
     steps = infile.read().splitlines()
@@ -17,31 +7,61 @@ steps = [(i.split(' ')[1], i.split(' ')[-3]) for i in steps]
 
 all_befores, all_afters = set(), set()
 befores_dict = defaultdict(set) ; afters_dict = defaultdict(set)
+
 for before, after in steps:
     all_befores.add(before) ; all_afters.add(after)
     befores_dict[after].add(before) ; afters_dict[before].add(after)
 
-start = (all_befores - all_afters).pop() ; end = (all_afters - all_befores).pop() ; all_letters = all_befores | all_afters
+all_letters = all_befores | all_afters
+
 
 def sort_alpha(nexts):
-    alpha_order = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    ordered = sorted({alpha_order.index(i): i for i in nexts}.items())
-    return [i[1] for i in ordered]
+    return sorted(set(nexts))
 
-def get_alpha_idx(char):
+
+def get_wait_time(ltr):
+    wait_window = 60
     alpha_order = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    return alpha_order.index(char)
+    return alpha_order.index(ltr) + wait_window + 1
+
+
+class Worker():
+    def __init__(self):
+        self.ltr = False
+        self.cnt = 0
+
+
+workers = [Worker(), Worker(), Worker(), Worker(), Worker()]
 
 order = []
+in_progress = set()
+second_tick = 0
 
 while len(order) < len(all_letters):
+    for worker in workers:
+        if worker.ltr and worker.cnt == 0:
+            order.append(worker.ltr)
+            in_progress.remove(worker.ltr)
+            worker.ltr = False
     possibles = []
     for x in all_letters:
-        if x not in order and all(i in order for i in befores_dict[x]):
+        if x not in order and x not in in_progress and all(i in order for i in befores_dict[x]):
             possibles.append(x)
+
     possibles = sort_alpha(possibles)
 
-    order.append(possibles[0])
+    for worker in workers:
+        if not worker.ltr and possibles:
+            worker.ltr = possibles.pop(0)
+            worker.cnt = get_wait_time(worker.ltr)
+            in_progress.add(worker.ltr)
 
-print(sum([get_alpha_idx(o) + 60 for o in order]))
+    if len(order) == len(all_letters):
+        break
+    for worker in workers:
+        if worker.ltr:
+            worker.cnt -= 1
 
+    second_tick += 1
+
+print(second_tick)
